2ee35d1b53d0ac6c376f6588210bf71b62ac4322
Raphael Hetherington <raph37heth@gmail.com>
2020-03-09 11:53:54 +0000

    added HOCs

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,46 +1,50 @@
-import React, { Component } from 'react'
-import { connect } from "react-redux"
-import * as actions from "actions"
-
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import * as actions from "actions";
+import requireAuth from "components/requireAuth"
 class CommentBox extends Component {
-    state = {
-        comment: ""
-    };
+  state = {
+    comment: ""
+  };
 
-    handleChange = e => {
-        this.setState({
-            comment: e.target.value
-        })
-    }
+  // our component was rendered
+  
 
-    handleSubmit = e => {
-        e.preventDefault();
+  handleChange = e => {
+    this.setState({
+      comment: e.target.value
+    });
+  };
 
-        // TODO - call an action creator 
-        // and save the comment
-        this.props.saveComment(this.state.comment)
-        this.setState({
-            comment: ""
-        })
-    }
+  handleSubmit = e => {
+    e.preventDefault();
 
-    render() {
-        return (
-            <div>
-            <form onSubmit={this.handleSubmit}>
-                <h4>Add a comment</h4>
-                <textarea onChange={this.handleChange} value={this.state.comment}/>
-                <div>
-                    <button>
-                        Submit Comment
-                    </button>
-                </div>
-            </form>
-            <button className="fetch-comments" onClick={this.props.fetchComments}>Fetch Comments</button>
-            </div>
-        )
-    }
+    // TODO - call an action creator
+    // and save the comment
+    this.props.saveComment(this.state.comment);
+    this.setState({
+      comment: ""
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <h4>Add a comment</h4>
+          <textarea onChange={this.handleChange} value={this.state.comment} />
+          <div>
+            <button>Submit Comment</button>
+          </div>
+        </form>
+        <button className="fetch-comments" onClick={this.props.fetchComments}>
+          Fetch Comments
+        </button>
+      </div>
+    );
+  }
 }
 
 
-export default connect(null, actions)(CommentBox)
+
+export default connect(null, actions)(requireAuth(CommentBox));
--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom";
 import App from "components/App";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
-import reducers from "reducers"
-import Root from "Root"
+import { BrowserRouter, Route } from "react-router-dom";
+import reducers from "reducers";
+import Root from "Root";
 
 ReactDOM.render(
-    <Root>
-        <App />
-    </Root>,
-document.getElementById("root"));
+  <Root>
+    <BrowserRouter>
+      <Route path="/" component={App} />
+    </BrowserRouter>
+  </Root>,
+  document.getElementById("root")
+);
